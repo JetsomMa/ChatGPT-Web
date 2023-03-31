628c5ad9cb62fa2bb03a1c0c20ddebcf79d4d11d
mashaojie <mashaojie@pinefield-inc.com>
2023-04-01 02:50:37 +0800

    perf: 优化代码，实现一键打包

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://download.mashaojie.cn/image/avatar.jpg',
+      name: 'Ma Shaojie',
+      description: '到我的 <a href="https://mashaojie.cn" class="text-blue-500" target="_blank" >博客</a>',
     },
   }
 }
