0e86e388ab522178a631742a2f2bcca7aabc40e1
mashaojie.club <mashaojie@pinefield-inc.com>
2023-04-07 01:24:17 +0800

    perf: 优化了设备识别

--- docker.example.js
+++ docker.example.js
@@ -2,12 +2,13 @@ const { execSync } = require('child_process')
 const colors = require('colors-console')
 
 const version = '0.1.12'
+const dockerHub = 'xxx.xxx.xxx.xxx:5000'
 
 console.error(colors('blue', `生产环境：准备生成镜像${version}`))
 console.error(colors('blue', '开始打包镜像'))
-runCmd(`docker build --platform=linux/amd64 -t 118.195.236.91:5000/chatgpt-web:${version} .`)
+runCmd(`docker build --platform=linux/amd64 -t ${dockerHub}/chatgpt-web:${version} .`)
 console.error(colors('blue', '开始上传镜像'))
-runCmd(`docker push 118.195.236.91:5000/chatgpt-web:${version}`)
+runCmd(`docker push ${dockerHub}/chatgpt-web:${version}`)
 console.error(colors('blue', '镜像上传完毕！！！'))
 console.error(colors('blue', '请执行以下命令启动镜像'))
 console.error(colors('blue', `docker pull 127.0.0.1:5000/chatgpt-web:${version}`))
