3c21a5055d0b96366903b7117f87dd4cefd47bbf
mashaojie <mashaojie@pinefield-inc.com>
2023-06-25 11:36:00 +0800

    perf: 禁用new bing 回复语音识别

--- src/views/chat/components/Header/options.ts
+++ src/views/chat/components/Header/options.ts
@@ -2,7 +2,7 @@ export const querymethodsOptions = [
   { label: 'ChatGPT3.5', value: 'ChatGPT' },
   { label: 'ChatGPT3.5大文本', value: 'ChatGPT16K' },
   { label: 'Claude[免费]', value: 'Claude' },
-  { label: 'New Bing[免费]', value: '浏览器' },
+  // { label: 'New Bing[免费]', value: '浏览器' },
   // { label: '影视', value: '影视' },
   // { label: '音乐', value: '音乐', disabled: true },
   { label: '画画[MDJ]', value: '画画', disabled: false },
